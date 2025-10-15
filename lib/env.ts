69ceb5dc5a02fbed5546669f18592bdcabdb15c8
varunchauhan12 <varunchauhan_mc24b06_058@dtu.ac.in>
2025-10-15 22:41:40 +0530

    completed authentication and implemented arcjet security

--- lib/env.ts
+++ lib/env.ts
@@ -9,6 +9,7 @@ export const env = createEnv({
     AUTH_GITHUB_ID: z.string().min(1),
     AUTH_GITHUB_SECRET: z.string().min(1),
       RESEND_API_KEY: z.string().min(1),
+      ARCJET_API_KEY: z.string().min(1),
   },
 
   // For Next.js >= 13.4.4, you only need to destructure client variables:
