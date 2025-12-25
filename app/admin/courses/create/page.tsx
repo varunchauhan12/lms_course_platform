5fefecb4d0b2c5017e855d46a713ecc0ed90039e
varunchauhan12 <varunchauhan_mc24b06_058@dtu.ac.in>
2025-12-25 21:27:54 +0530

    course Creation page - 1 ( made prisma schema and created route )

---
+++ app/admin/courses/create/page.tsx
@@ -0,0 +1,3 @@
+export default  function CreateCoursePage() {
+  return <div>Hey</div>
+}
--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
 export default function CoursesPage() {
-  return <div>Courses Page</div>
+  return (
+    <>
+      <div className={"flex items-center justify-between"}>
+        <h1 className={"text-2xl font-bold"}>Your Courses</h1>
+        <Link href={"/admin/courses/create"} className={buttonVariants()}>
+          Create Course
+        </Link>
+      </div>
+      <div>
+        Here are all the courses
+      </div>
+    </>
+  );
 }
