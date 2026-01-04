ebc6ec76c2f135f42c573ec3160f098190e0ae93
varunchauhan12 <varunchauhan_mc24b06_058@dtu.ac.in>
2026-01-04 23:37:33 +0530

    upload file -2

--- components/file-uploaded/uploader.tsx
+++ components/file-uploaded/uploader.tsx
@@ -1,15 +1,77 @@
 "use client";
 
-import React, { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+import React, { useCallback, useState } from "react";
+import { FileRejection, useDropzone } from "react-dropzone";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { RenderEmptyState } from "@/components/file-uploaded/RenderState";
-
+import { toast } from "sonner";
+import { v4 as uuidv4 } from "uuid";
+interface UploaderProps {
+  id: string | null;
+  file: File | null;
+  uploading: boolean;
+  progress: number;
+  key?: string;
+  isDeleting: boolean;
+  error: boolean;
+  ObjectUrl?: string;
+  fileType: "image" | "video";
+}
 const Uploader = () => {
+  const [fileState, setFileState] = useState<UploaderProps>({
+    id: null,
+    file: null,
+    uploading: false,
+    progress: 0,
+    isDeleting: false,
+    error: false,
+    fileType: "image",
+  });
+  function uploadFile(file: File) {
+    // Upload file logic here
+    setFileState((prev) => ({
+      ...prev,
+      uploading: true,
+      progress: 0,
+    }));
+  }
+
+  function FileRejection(filerejection: FileRejection[]) {
+    if (filerejection.length) {
+      const tooManyFiles = filerejection.find(
+        (rejection) => rejection.errors[0].code === "too-many-files",
+      );
+
+      const fileTooLarge = filerejection.find(
+        (rejection) => rejection.errors[0].code === "file-too-large",
+      );
+
+      if (fileTooLarge) {
+        toast.error("File is too large. Maximum size is 5MB.");
+        return;
+      }
+
+      if (tooManyFiles) {
+        toast.error("You can only upload one file at a time.");
+        return;
+      }
+    }
+  }
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    console.log(acceptedFiles);
-    // Do something with the files
+    if (acceptedFiles.length > 0) {
+      const file = acceptedFiles[0];
+      setFileState({
+        file: file,
+        uploading: false,
+        progress: 0,
+        ObjectUrl: URL.createObjectURL(file),
+        error: false,
+        id: uuidv4(),
+        isDeleting: false,
+        fileType: "image",
+      });
+    }
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
